ff1f312f088332789c497287713f666aeddc7daf
Reuben Sunday <hello@reubensunday.com>
2023-02-25 18:43:25 +0100

    updated scroll action

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from utils.helper_class import WAScrapper
 was = WAScrapper()
+was.SCROLL_COUNT = 10
 was.fetch_messages()
--- utils/helper_class.py
+++ utils/helper_class.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
@@ -8,16 +9,21 @@ import time
 import sys
 from utils.settings import CHROME_PROFILE, CHROME_DRIVER_PATH
 WAIT_TIME = 10
+SCROLL_COUNT = 4
 
 
 class WAScrapper:
+    '''WhatsApp web scrapper class'''
+
     def __init__(self) -> None:
+        '''Initialize module'''
         self.options = webdriver.ChromeOptions()
         self.options.add_argument(CHROME_PROFILE)
         self.service = Service(CHROME_DRIVER_PATH)
         self.driver = WebDriver(service=self.service, options=self.options)
         self.driver.get("https://web.whatsapp.com/")
         self.driver.maximize_window()
+        self.SCROLL_COUNT = SCROLL_COUNT
         try:
             with open('groups.txt', 'r') as f:
                 self.group_names = f.readlines()
@@ -28,19 +34,31 @@ class WAScrapper:
     def send_message(self, contact_name):
         pass
 
+    def perform_scroll(self):
+        '''perform scroll action'''
+        chat_container = self.driver.find_element(
+            By.XPATH, '//*[@data-testid="conversation-panel-messages"]')
+        for _ in range(self.SCROLL_COUNT):
+            chat_container.send_keys(Keys.CONTROL + Keys.HOME)
+            # wait for chats to load
+            time.sleep(2)
+
     def fetch_messages(self):
+        '''collect chat history'''
         input('Please click enter after loading completes: ')
         time.sleep(2)
         for group_name in self.group_names:
             group = self.driver.find_element(
                 By.XPATH, f'//*[@title="{group_name.strip()}"]')
             group.click()
+            # wait until chat are loaded
             WebDriverWait(self.driver, WAIT_TIME).until(
                 EC.presence_of_element_located((By.CLASS_NAME, '_27K43')))
-            self.driver.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight);")
+            # perform a scroll event
+            self.perform_scroll()
             chats = self.driver.find_elements(By.CLASS_NAME, '_27K43')
             with open('chats.txt', 'a') as f:
                 for chat in chats:
-                    f.writelines(chat.text + '\n')
+                    f.writelines(chat.text + '\n\n\n')
+        print('Completed')
         time.sleep(10)
